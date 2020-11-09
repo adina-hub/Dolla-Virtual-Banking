6d8d8e2cb9f3b48fc2331a6952da1adb16ff288d
adina-hub <64439268+adina-hub@users.noreply.github.com>
2020-11-09 21:59:44 +0200

    Added 'Button' component

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward,
         ArrowRight } from './HeroElements'
 import Video from '../../videos/video.mp4'
+import {Button} from '../ButtonElement'
 
 function HeroSection() {
     const [hover,setHover] = useState(false)
@@ -19,7 +20,7 @@ function HeroSection() {
                 <HeroH1>Virtual Banking Made Easy</HeroH1>
                 <HeroP>Sign up for a new account today and receive $250 in credits towards your next payment.</HeroP>
                 <HeroBtnWrapper>
-                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover} primary="true" dark="true">
                         Get Started {hover ? <ArrowForward /> :  <ArrowRight />}
                     </Button>
                 </HeroBtnWrapper>
