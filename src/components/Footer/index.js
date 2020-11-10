004852ed63099071aaf5f36b23b4debf35570175
adina-hub <64439268+adina-hub@users.noreply.github.com>
2020-11-10 22:50:20 +0200

    Styled Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import {FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaYoutube} from 'react-icons/fa'
+import {FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle,
+        FooterLink, SocialMedia, SocialMediaWrap, SocialIconLink, SocialIcons, WebsiteRights, SocialLogo} from './FooterElements'
 
 const Footer = () => {
     return (
@@ -8,40 +11,62 @@ const Footer = () => {
                     <FooterLinksWrapper>
                         <FooterLinkItems>
                             <FooterLinkTitle>About Us</FooterLinkTitle>
-                    <FooterLink to="/signin">How it works</FooterLink>                            <FooterLink to="/signin">Testimonials</FooterLink>
-                    <FooterLink to="/signin">Careers</FooterLink>
-                    
-                    <FooterLink to="/signin">Investors</FooterLink>
-                            <FooterLink to="/signin">Terms of Service</FooterLink>
+                            <FooterLink to="/">How it works</FooterLink>                            
+                            <FooterLink to="/">Testimonials</FooterLink>
+                            <FooterLink to="/">Careers</FooterLink>
+                            <FooterLink to="/">Investors</FooterLink>
+                            <FooterLink to="/">Terms of Service</FooterLink>
                         </FooterLinkItems>
                         <FooterLinkItems>
-                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to="/signin">How it works</FooterLink>
-                            <FooterLink to="/signin">Testimonials</FooterLink>
-                            <FooterLink to="/signin">Careers</FooterLink>
-                            <FooterLink to="/signin">Investors</FooterLink>
-                            <FooterLink to="/signin">Terms of Service</FooterLink>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                            <FooterLink to="/">Contact</FooterLink>
+                            <FooterLink to="/">Support</FooterLink>
+                            <FooterLink to="/">Destinations</FooterLink>
+                            <FooterLink to="/">Sponsorships</FooterLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                     <FooterLinksWrapper>
                         <FooterLinkItems>
-                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to="/signin">How it works</FooterLink>
-                            <FooterLink to="/signin">Testimonials</FooterLink>
-                            <FooterLink to="/signin">Careers</FooterLink>
-                            <FooterLink to="/signin">Investors</FooterLink>
-                            <FooterLink to="/signin">Terms of Service</FooterLink>
+                            <FooterLinkTitle>Videos</FooterLinkTitle>
+                            <FooterLink to="/">Submit Video</FooterLink>
+                            <FooterLink to="/">Ambassadors</FooterLink>
+                            <FooterLink to="/">Agency</FooterLink>
+                            <FooterLink to="/">Influencer</FooterLink>
                         </FooterLinkItems>
                         <FooterLinkItems>
-                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to="/signin">How it works</FooterLink>
-                            <FooterLink to="/signin">Testimonials</FooterLink>
-                            <FooterLink to="/signin">Careers</FooterLink>
-                            <FooterLink to="/signin">Investors</FooterLink>
-                            <FooterLink to="/signin">Terms of Service</FooterLink>
+                            <FooterLinkTitle>Social Media</FooterLinkTitle>
+                            <FooterLink to="/">Instagram</FooterLink>
+                            <FooterLink to="/">Facebook</FooterLink>
+                            <FooterLink to="/">Youtube</FooterLink>
+                            <FooterLink to="/">Twitter</FooterLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'>
+                            dolla
+                        </SocialLogo>
+                        <WebsiteRights>dolla {new Date().getFullYear()} All rights reserved.</WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href='/' target="_blank" aria-label="Facebook">
+                                <FaFacebook/>
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target="_blank" aria-label="Instagram">
+                                <FaInstagram/>
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target="_blank" aria-label="Youtube">
+                                <FaYoutube/>
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target="_blank" aria-label="Twitter">
+                                <FaTwitter/>
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target="_blank" aria-label="Linkein">
+                                <FaLinkedin/>
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
             </FooterWrap>
         </FooterContainer>
     )
