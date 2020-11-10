fd1c617a8bd3a603d83f81dc95783735fcdd9a0c
finy_ber <finy_ber@yahoo.com>
2020-11-10 12:17:14 +0200

    Created Sign In page and added React Routes to website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Home from './pages'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import SignInPage from './pages/signin';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>\
-      <Home />
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/signin" component={SignInPage} exact />
+      </Switch>
     </Router>
   );
 }
